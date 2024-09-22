62f5c1efd374dcffb3e1f72b641c705bc266978a
Rownak Abdullah <rownakabdullahomi@gmail.com>
2024-09-23 00:56:04 +0600

    card button2 31 donation done with responsive

--- main.js
+++ main.js
@@ -1,19 +1,44 @@
 let availableValue = parseFloat(document.getElementById("available-value").innerText);
-let inputValue = 0;
 
-// Event Handling for Card-1 Button
+// Event Handling for Card-1 Donation Button
 document.getElementById("card1-btn").addEventListener("click", function () {
-    inputValue += getInputValueInFloat("card1-input");
-    addValueToSpan("card1-donation", inputValue);
+    const inputValue = getInputValueInFloat("card1-input");
+    const existingValue = getExistingSpanValueInFloat("card1-donation");
+    const totalDonation = inputValue + existingValue;
+    addValueToSpan("card1-donation", totalDonation);
     availableValue -= inputValue;
     addValueToSpan("available-value", availableValue);
 });
+
+// Event Handling for Card-2 Donation Button
+document.getElementById("card2-btn").addEventListener("click", function () {
+    inputValue = getInputValueInFloat("card2-input");
+    const existingValue = getExistingSpanValueInFloat("card2-donation");
+    const totalDonation = inputValue + existingValue;
+    addValueToSpan("card2-donation", totalDonation);
+    availableValue -= inputValue;
+    addValueToSpan("available-value", availableValue);
+});
+
+// Event Handling for Card-3 Donation Button
+document.getElementById("card3-btn").addEventListener("click", function () {
+    inputValue = getInputValueInFloat("card3-input");
+    const existingValue = getExistingSpanValueInFloat("card3-donation");
+    const totalDonation = inputValue + existingValue;
+    addValueToSpan("card3-donation", totalDonation);
+    availableValue -= inputValue;
+    addValueToSpan("available-value", availableValue);
+});
+
 
 function addValueToSpan(id, inputValue) {
     const donationSpan = document.getElementById(id);
     donationSpan.innerText = inputValue.toFixed(2);
 }
 
+function getExistingSpanValueInFloat(id){
+    return parseFloat(document.getElementById(id).innerText);
+}
 function getInputValueInFloat(id) {
     return parseFloat(document.getElementById(id).value);
 }
