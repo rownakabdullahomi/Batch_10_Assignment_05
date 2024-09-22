01bc221452b7c3dd5bb16c2fb987e76545d4f792
Rownak Abdullah <rownakabdullahomi@gmail.com>
2024-09-23 03:02:00 +0600

    history and donation toggle done, get history from all card done

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 let availableValue = parseFloat(document.getElementById("available-value").innerText);
+const historyBtn = document.getElementById("history-btn");
+const donationBtn = document.getElementById("donation-btn");
+const historySection = document.getElementById("history-section");
+const card3Title = document.getElementById("card3-title").innerText;
+const card2Title = document.getElementById("card2-title").innerText;
+const card1Title = document.getElementById("card1-title").innerText;
 
 // Event Handling for Card-1 Donation Button
 document.getElementById("card1-btn").addEventListener("click", function () {
@@ -8,7 +14,10 @@ document.getElementById("card1-btn").addEventListener("click", function () {
     addValueToSpan("card1-donation", totalDonation);
     availableValue -= inputValue;
     addValueToSpan("available-value", availableValue);
+    getHistory(inputValue, card1Title);
 });
+
+
 
 // Event Handling for Card-2 Donation Button
 document.getElementById("card2-btn").addEventListener("click", function () {
@@ -18,7 +27,10 @@ document.getElementById("card2-btn").addEventListener("click", function () {
     addValueToSpan("card2-donation", totalDonation);
     availableValue -= inputValue;
     addValueToSpan("available-value", availableValue);
+    getHistory(inputValue, card2Title);
 });
+
+
 
 // Event Handling for Card-3 Donation Button
 document.getElementById("card3-btn").addEventListener("click", function () {
@@ -28,7 +40,28 @@ document.getElementById("card3-btn").addEventListener("click", function () {
     addValueToSpan("card3-donation", totalDonation);
     availableValue -= inputValue;
     addValueToSpan("available-value", availableValue);
+    getHistory(inputValue, card3Title);
+    
+    
+
 });
+
+
+
+historyBtn.addEventListener("click", function(){
+    historyBtn.classList.add("bg-primary", "font-semibold", "text-txt11")
+    donationBtn.classList.remove("bg-primary", "font-semibold", "text-txt11")
+    donationBtn.classList.add("font-medium", "text-[#111111B3]")
+
+    
+});
+donationBtn.addEventListener("click", function(){
+    historyBtn.classList.remove("bg-primary", "font-semibold", "text-txt11")
+    donationBtn.classList.add("bg-primary", "font-semibold", "text-txt11")
+    donationBtn.classList.remove("font-medium", "text-[#111111B3]")
+});
+
+
 
 
 function addValueToSpan(id, inputValue) {
@@ -41,5 +74,18 @@ function getExistingSpanValueInFloat(id){
 }
 function getInputValueInFloat(id) {
     return parseFloat(document.getElementById(id).value);
+}
+
+function getDateTime(){
+    return Date().toString();
+}
+
+function getHistory(inputValue, cardTitle){
+    historySection.innerHTML += `
+    <div class="navbar rounded-lg border-2 border-[#111111A] flex-col items-start pl-10 py-5 mb-5">
+        <p class="font-bold text-[20px] text-txt11"> ${inputValue.toFixed(2)} Taka is Donated for ${cardTitle}, Bangladesh</p>
+        <p class="font-light text-[16px] text-txt11"> Date: ${getDateTime()}</p>
+    </div>
+    `
 }
 
