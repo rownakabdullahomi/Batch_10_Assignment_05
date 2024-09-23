7ed317dfe23d0b6385808c237904017f7f52b2e8
Rownak Abdullah <rownakabdullahomi@gmail.com>
2024-09-23 14:50:02 +0600

    Input and Amount Validation for Card-1 done

--- main.js
+++ main.js
@@ -10,12 +10,20 @@ const mainSection = document.getElementById("main");
 // Event Handling for Card-1 Donation Button
 document.getElementById("card1-btn").addEventListener("click", function () {
     const inputValue = getInputValueInFloat("card1-input");
+    
+    //Input and Amount Validation for Card-1
+    if (inputValidation(inputValue) === false) {
+        clearField("card1-input")
+        return;
+    }
+
     const existingValue = getExistingSpanValueInFloat("card1-donation");
     const totalDonation = inputValue + existingValue;
     addValueToSpan("card1-donation", totalDonation);
     availableValue -= inputValue;
     addValueToSpan("available-value", availableValue);
     getHistory(inputValue, card1Title);
+    clearField("card1-input");
 });
 
 
@@ -96,5 +104,20 @@ function getHistory(inputValue, cardTitle) {
     `
     // historySection.appendChild(historyDiv); // It shows the Last Input as Last Record
     historySection.insertBefore(historyDiv, historySection.firstChild); //Show the Last Input as First Record 
+}
+
+function inputValidation(inputValue) {
+    if (inputValue <= 0 || isNaN(inputValue)) {
+        alert("Please insert a valid positive number ..!!!");
+        return false;
+    }
+    else if(inputValue > availableValue){
+        alert("Available balance is low for this donation");
+        return false;
+    }
+}
+
+function clearField(id) {
+    document.getElementById(id).value = "";
 }
 
